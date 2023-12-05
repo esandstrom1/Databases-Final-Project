335f153707ee7e348c2d556ca23824f550308a2a
Ethan Sandstrom <esandstrom@Ethans-MBP-1247.lan>
2023-12-04 21:02:25 -0500

    Map and console with hardcoded output. Not formatted

--- test/app.py
+++ test/app.py
@@ -1,5 +1,7 @@
 # app.py
 from flask import Flask, render_template, request
+import subprocess
+from user_query import print_state
 
 app = Flask(__name__)
 
@@ -14,11 +16,21 @@ def process_click():
     y = data['y']
     # Call your Python function with the x and y coordinates
     result = your_function(x, y)
+    #result = result.replace('\n', '<br>')
     return {'result': result}
 
 def your_function(x, y):
     # Your Python logic based on the clicked coordinates
     # Replace this with your actual functionality
+    try:
+        # Call the "query.py" program and capture its output
+        #result = subprocess.check_output(["python3", "user_query.py"], universal_newlines=True)
+        result = print_state("Alaska", 2001)
+        #result = result.replace('\n', '<br>')
+        return result
+    except subprocess.CalledProcessError as e:
+        return f"Error: {e}"
+
     return f"Clicked at ({x}, {y})"
 
 if __name__ == '__main__':
