022297115dbf6bb6dc71bf0dffc1358aef6ae515
Ethan Sandstrom <esandstrom@Ethans-MBP-1248.lan>
2023-12-05 01:00:22 -0500

    Cociné🔥. Slider changes the console output. State coordinates begun with Alaska

--- ethans_folder/app.py
+++ ethans_folder/app.py
@@ -4,6 +4,8 @@ import sqlite3
 from user_query import print_state
 import subprocess
 
+global_state = "Ohio"
+
 app = Flask(__name__)
 
 
@@ -12,26 +14,33 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/on_click', methods=['POST'])
-def on_click():
+@app.route('/on_update', methods=['POST'])
+def on_update():
     data = request.get_json()
     x = data['x']
     y = data['y']
-    result = call_state(x, y)
+    year = data['year']
+    result = call_state(x, y, year)
     return {'result': result}
 
 
-def call_state(x, y):
+def call_state(x, y, year):
+    global global_state
+    if x >= 165 and x <= 218:
+        if y >= 165 and y <= 218:
+            state = "Alaska"
+            global_state = "Alaska"
+    if x == 0 and y == 0:
+        state = global_state
+
+
     try:
-        result = print_state("Ohio", "2005")
-
-
+        result = print_state(state, year)
         result_and_coordinates = f"Clicked at ({x}, {y})\n" + result
         return result_and_coordinates
     except subprocess.CalledProcessError as e:
         return f"Error: {e}"
 
-    return f"Clicked at ({x}, {y})"
 
 
 
