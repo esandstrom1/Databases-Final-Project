bc09478339b37d9adbe71727784ca84d203c783e
Julian Renton <jr362419@ohio.edu>
2023-12-04 22:24:17 -0500

    sliders done

--- streamlit/streamlit_test.py
+++ streamlit/streamlit_test.py
@@ -55,9 +55,9 @@ def main():
     st.title('United States Death Statistics \n 1999-2017')
 
     # years slider 
-    # st.subheader('Years')
-    years_range = st.slider('Years', value=[1999, 2017], min_value=1999,
-        max_value = 2017)
+    years_range = st.select_slider('Select a year:', options=[i for i in range(1999, 2018)])
+    display_values_range = st.slider('Select result range', value=[0,10],
+        min_value=0, max_value=10)
 
     # Display the image
     image = 'map.jpg'  # Replace 'your_image.jpg' with the path to your image file
